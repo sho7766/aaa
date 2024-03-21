bb8273e1d6fce3559ee29efdf0701fafee7ba26a
sho <sho7766@naver.com>
2024-03-21 17:57:07 +0900

    first commit

--- samyang/js/script.js
+++ samyang/js/script.js
@@ -1,4 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper_horizontal = new Swiper("#swiper-container-horizontal", {
+  direction: "horizontal",
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
@@ -11,4 +12,26 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+var swiper_vertical = new Swiper("#swiper-container-vertical", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+$("header.white .close").click(function () {
+  $(".drop_box").css("display", "none");
+});
+$(".menu .lines").click(function () {
+  $(".drop_box").css("display", "block");
 });
