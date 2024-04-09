5c84aedc9c99d2b961e351394669001d0f71b55c
sho7766 <sho7766@naver.com>
2024-04-10 00:27:28 +0900

    first commit

--- ottogi/js/index.js
+++ ottogi/js/index.js
@@ -1,40 +1,30 @@
-let bg = document.querySelector("header");
+let header = document.querySelector("header");
 let inner = document.querySelector(".inner");
 let gnb = document.querySelectorAll(".gnb>li:not(:last-child)");
 let sub = document.querySelectorAll(".sub");
 
-$(".gnb > li:last-child").mouseenter(function () {
-  $(".gnb > li:last-child a").css(
-    "background",
-    "url(images/btn_menu_w_hover.svg)center no-repeat"
-  );
-});
-$(".gnb > li:last-child").mouseleave(function () {
-  $(".gnb > li:last-child a").css(
-    "background",
-    "url(images/btn_menu_w.svg)center no-repeat"
-  );
-});
 $(gnb).mouseenter(function () {
   $(".gnb>li>a").addClass("on");
   $(".lang a").css({ color: "#333", textShadow: "none" }).addClass("on");
-  $(bg).addClass("on");
+  $(header).addClass("on");
   $(sub).css("height", "358px");
   $(".gnb > li:last-child a").css(
     "background",
     "url(images/btn_menu_hover.svg)center no-repeat"
   );
+  $(header).addClass("off");
 });
-$(gnb).mouseleave(function () {
+$(header).mouseleave(function () {
   $(".gnb>li>a").removeClass("on");
   $(".lang a").css("color", "#fff");
   $(".lang a").removeClass("on");
   $(sub).css("height", "0");
-  $(bg).removeClass("on");
+  $(header).removeClass("on");
   $(".gnb > li:last-child a").css(
     "background",
     "url(images/btn_menu_w.svg)center no-repeat"
   );
+  $(header).removeClass("off");
 });
 
 // 헤더 어디든 호버해도 서브메뉴가 사라지지 않도록 하기
