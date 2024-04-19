2f4aa79ac3ec93a525507481b9a5a45e6d87f647
sho7766 <sho7766@naver.com>
2024-04-19 16:23:04 +0900

    first commit

--- ottogi/js/index.js
+++ ottogi/js/index.js
@@ -85,6 +85,180 @@ $(".main .play").click(function () {
 });
 //
 
+// info섹션 스크롤 이벤트
+let scrollDown = gsap.timeline();
+scrollDown.from(".info .scroll_down ", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: scrollDown,
+  trigger: ".info",
+  start: "top 80%",
+});
+let infoTit = gsap.timeline();
+infoTit.from(".info .main_tit ", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: infoTit,
+  trigger: ".info",
+  start: "top 60%",
+});
+// let one = gsap.timeline();
+// one.from(".info .one", {
+//   y: 100,
+//   autoAlpha: 0,
+//   duration: 1,
+// });
+// ScrollTrigger.create({
+//   animation: one,
+//   trigger: ".info",
+//   start: "top 40%",
+// });
+let two = gsap.timeline();
+two.from(".info .two", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: two,
+  trigger: ".info .one",
+  start: "top 10%",
+});
+let svg2 = gsap.timeline();
+two.from(".info .svg2", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 0.5,
+});
+ScrollTrigger.create({
+  animation: svg2,
+  trigger: ".info .two",
+  start: "top 50%",
+});
+let three = gsap.timeline();
+three.from(".info .three", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 0.5,
+});
+ScrollTrigger.create({
+  animation: three,
+  trigger: ".info .two",
+  start: "top 20%",
+});
+//
+
+// global 섹션 스크롤 이벤트
+let global1 = gsap.timeline();
+global1.from(".global .sub_tit", {
+  y: 50,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: global1,
+  trigger: ".global",
+  start: "top 70%",
+});
+let global2 = gsap.timeline();
+global2.from(".global .main_tit", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: global2,
+  trigger: ".global",
+  start: "top 50%",
+});
+let global3 = gsap.timeline();
+global3.from(".global .desc", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: global3,
+  trigger: ".global",
+  start: "top 30%",
+});
+let global4 = gsap.timeline();
+global4.from(".global .btn_more", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: global4,
+  trigger: ".global",
+  start: "top 20%",
+});
+
+const popAni = document.addEventListener("DOMContentLoaded", function () {
+  var imgWraps = document.querySelectorAll(".global_cont .img_wrap li");
+  var imgWrapArray = Array.prototype.slice.call(imgWraps); // NodeList를 배열로 변환
+
+  // 배열을 섞기 위한 함수
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // 아직 섞을 요소가 남아있는 동안
+    while (0 !== currentIndex) {
+      // 남아있는 요소 중에서 무작위 인덱스 선택
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // 현재 요소와 무작위로 선택된 요소를 교환
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  // 배열을 섞은 후 요소에 클래스 추가하여 나타나도록 함
+  var shuffledImgWrapArray = shuffle(imgWrapArray);
+  shuffledImgWrapArray.forEach(function (imgWrap, index) {
+    setTimeout(function () {
+      imgWrap.classList.add("on");
+    }, index * 50);
+  });
+});
+
+// esg섹션 스크롤 이벤트
+let esgTit = gsap.timeline();
+esgTit.from(".esg .main_tit", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: esgTit,
+  trigger: ".esg",
+  start: "top 90%",
+});
+let esgSlide = gsap.timeline();
+esgSlide.from(".esg .swiper", {
+  y: 100,
+  autoAlpha: 0,
+  duration: 1,
+});
+ScrollTrigger.create({
+  animation: esgSlide,
+  trigger: ".esg",
+  start: "top 50%",
+});
+//
+
 // esg 섹션 슬라이드
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
